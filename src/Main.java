231aed355985d3f6217534c0d7bdb142dc24b200
clorente2001 <clorente2001@gmail.com>
2020-06-10 17:28:32 +0200

    need to improve the function to modify the contact information

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,67 @@
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-    static Connection c = null;
-    static Statement st = null;
+    private static Connection c;
+    private static Statement st;
+    private static ResultSet rs;
+    private static Scanner s = new Scanner(System.in);
 
-    public static void main( String args[] ) throws Exception {
-            Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:E:/SQLite Databases/agenda.sqlite");
-            System.out.println("Opened database successfully");
+    public static void main(String args[]) throws Exception {
+        Class.forName("org.sqlite.JDBC");
+        c = DriverManager.getConnection("jdbc:sqlite:E:/SQLite Databases/agenda.sqlite");
 
-            // TABLES
-            createTables();
+        boolean keep = true;
+        // TABLES
+        createTables();
+        // INSERTS
+        insertData();
 
-            /* INSERTS */
-            insertData();
+        while (keep) {
+            System.out.println("Que acción desea realizar? (consultar, insertar, modificar o eliminar) ");
+            String action = s.next();
 
-            // SELECTS
+            if (action.equalsIgnoreCase("consultar")) {
+                System.out.println("Desea listar todos los contactos o consultar uno en concreto? (todos, concreto) ");
+                String list = s.next();
+                if (list.equalsIgnoreCase("todos"))
+                    selectAll();
+                else if (list.equalsIgnoreCase("concreto")) {
+                    System.out.println("Introduce el DNI del contacto: ");
+                    selectOne(s.next());
+                }
+            }
+            else if (action.equalsIgnoreCase("insertar")) {
+                insert();
+            }
+            else if (action.equalsIgnoreCase("modificar")) {
+                System.out.println("DNI del contacto: ");
+                String DNI = s.next();
+                selectOne(DNI);
+                modify(DNI);
+            }
+            else if (action.equalsIgnoreCase("eliminar")) {
+                System.out.println("DNI del contacto: ");
+                delete(s.next());
+            }
+            else
+                System.out.println("Acción incorrecta");
 
-            // GENERAL
+            System.out.println("Desea continuar? (si, no)");
+            if (!s.next().equalsIgnoreCase("si")) {
+                keep = false;
+                System.out.println("Adiós.");
+            }
+        }
 
-            selectAll();
-
-            // CONCRET
-
-            /*rs = st.executeQuery("SELECT * FROM CONTACTOS;");
-
-            rs.close();
-            st.close();
-            c.close();
-            */
-
+        rs.close();
+        st.close();
+        c.close();
     }
 
-    static void createTables() throws Exception {
+    private static void createTables() throws Exception {
         st = c.createStatement();
 
         st.execute("DROP TABLE IF EXISTS CONTACTOS");
@@ -54,12 +83,12 @@ public class Main {
         // TELEFONOS
         sql = "CREATE TABLE TELEFONOS " +
                 "(DNI VARCHAR(9) NOT NULL," +
-                " Telefonos INT NOT NULL, " +
+                " Telefono INT NOT NULL, " +
                 " FOREIGN KEY(DNI) REFERENCES CONTACTOS (DNI) ON UPDATE CASCADE ON DELETE CASCADE)";
         st.execute(sql);
     }
 
-    static void insertData() throws Exception {
+    private static void insertData() throws Exception {
         // CONTACTOS
         st.execute("INSERT INTO CONTACTOS VALUES ('48584159A', 'Messi', 'Barcelona');");
         st.execute("INSERT INTO CONTACTOS VALUES ('12345678W', 'Jero', 'Mallorca');");
@@ -75,8 +104,8 @@ public class Main {
         st.execute("INSERT INTO TELEFONOS VALUES ('87654321Q', 671020406);");
     }
 
-    static void selectAll() throws Exception {
-        ResultSet rs = st.executeQuery( "SELECT DNI, NOMBRE, DIRECCION FROM CONTACTOS ORDER BY DNI DESC;" );
+    private static void selectAll() throws Exception {
+        rs = st.executeQuery("SELECT * FROM CONTACTOS;");
 
         System.out.println("+----------------+------------------------------+---------------------------------+");
         while (rs.next()) {
@@ -84,8 +113,103 @@ public class Main {
             String name = rs.getString("Nombre");
             String dir = rs.getString("Direccion");
 
-            System.out.printf("| DNI: %9s | Nombre: %-20s | Direccion: %-20s |\n", dni, name, dir);
+            System.out.printf("| DNI: %9s | Nombre: %-20s | Direccion: %-20s |\n",
+                    dni, name, dir);
         }
-        System.out.println("+----------------+------------------------------+---------------------------------+");
+        System.out.println("+----------------+------------------------------+---------------------------------+\n");
+    }
+
+    private static void selectOne(String DNI) throws Exception {
+        rs = st.executeQuery("SELECT C.DNI, C.Nombre, C.Direccion, E.Email, T.Telefono FROM CONTACTOS C" +
+                " LEFT JOIN EMAILS E ON C.DNI = E.DNI" +
+                " LEFT JOIN TELEFONOS T ON C.DNI = T.DNI" +
+                " WHERE C.DNI = '" + DNI + "';");
+
+        String dni = "";
+        String name = "";
+        String dir = "";
+        List<String> emails = new ArrayList<>();
+        List<Integer> telefonos = new ArrayList<>();
+
+        while (rs.next()) {
+            dni = rs.getString("DNI");
+            name = rs.getString("Nombre");
+            dir = rs.getString("Direccion");
+            if (!emails.contains(rs.getString("Email")))
+                emails.add(rs.getString("Email"));
+            if (!telefonos.contains(rs.getInt("Telefono")))
+                telefonos.add(rs.getInt("Telefono"));
+        }
+        System.out.printf("+----------------+------------------------------+---------------------------------+-----------------------------------------------------------+--------------------------------------------------------------+\n" +
+                        "| DNI: %9s | Nombre: %-20s | Direccion: %-20s | Email: %-50s | Telefono: %-50s |" +
+                        "\n+----------------+------------------------------+---------------------------------+-----------------------------------------------------------+--------------------------------------------------------------+\n",
+                dni, name, dir, emails, telefonos);
+    }
+
+    private static void insert() throws Exception {
+        System.out.println("DNI del contacto: ");
+        String dni = s.next();
+        System.out.println("Nombre del contacto: ");
+        String nombre = s.next();
+        System.out.println("Direccion del contacto: ");
+        String dir = s.next();
+
+        st.execute("INSERT INTO CONTACTOS VALUES ('" + dni + "', '" + nombre + "', '" + dir + "');");
+
+        System.out.println("Cúantos emails quieres añadir? ");
+        int nEmails = s.nextInt();
+
+        if (nEmails > 0) {
+            for (int i = 0; i < nEmails; i++) {
+                System.out.println("Email: ");
+                String email = s.next();
+                st.execute("INSERT INTO EMAILS VALUES ('" + dni + "', '" + email + "');");
+            }
+        }
+        else if (nEmails == 0)
+            System.out.println("Emails no añadidos.");
+        else
+            System.out.println("Número de emails no aceptado.");
+
+        System.out.println("Cúantos teléfonos quieres añadir? ");
+        int nTelf = s.nextInt();
+
+        if (nTelf > 0) {
+            for (int i = 0; i < nTelf; i++) {
+                System.out.println("Teléfono: ");
+                int telf = s.nextInt();
+                st.execute("INSERT INTO TELEFONOS VALUES ('" + dni + "', '" + telf + "');");
+            }
+        }
+        else if (nTelf == 0)
+            System.out.println("Teléfonos no añadidos.");
+        else
+            System.out.println("Número de teléfonos no aceptado.");
+
+        System.out.println("Contacto añadido correctamente.");
+
+    }
+
+    private static void modify(String DNI) throws Exception {
+        System.out.println("Nuevo nombre: ");
+        String nombre = s.next();
+        System.out.println("Nueva direccion: ");
+        String direccion = s.next();
+
+        st.executeUpdate("UPDATE CONTACTOS SET Nombre = '" + nombre + "', Direccion = '" + direccion + "' WHERE DNI = '" + DNI + "';");
+    }
+
+    private static void delete(String DNI) throws Exception {
+        System.out.println("Estás seguro de eliminar este contacto? Esta acción será irreversible (si, no)");
+        String conf = s.next();
+
+        if (conf.equalsIgnoreCase("si")) {
+            st.execute("DELETE FROM CONTACTOS WHERE DNI = '" + DNI + "';");
+            System.out.println("Contacto eliminado correctamente!");
+        }
+        else if (conf.equalsIgnoreCase("no"))
+            System.out.println("Contacto no eliminado!");
+        else
+            System.out.println("Confirmación errónea, contacto no eliminado");
     }
 }
